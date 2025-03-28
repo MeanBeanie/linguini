d9ee719e5cf2b8618f77a9f7d7eac261a048fe11
MeanBeanie <itsawildveg@gmail.com>
2025-03-28 10:13:24 -0700

    Updated to use marinara as a windower

--- linguini.h
+++ linguini.h
@@ -6,19 +6,6 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
-#ifdef USE_SDL
-#include <SDL2/SDL.h>
-#endif
-
-#ifdef USE_X11
-#include <X11/Xlib.h>
-#endif
-
-#ifdef USE_WAYLAND
-#include <wayland-client.h>
-#include "libReqs/xdg-shell-client-protocol.h"
-#endif
-
 // -- STRUCTS/VARS --
 
 // channels is currently only utilised for image processing
@@ -42,49 +29,6 @@ typedef struct {
 	FT_Face face;
 	int error;
 } linguini_freetypeContext;
-
-#ifdef USE_SDL
-typedef struct {
-	SDL_Window* window;
-	SDL_Renderer* renderer;
-	SDL_Texture* windowTexture;
-	size_t width;
-	size_t height;
-	int isOpen;
-} linguini_SDLContext;
-#endif
-
-#ifdef USE_X11
-typedef struct {
-	Display* display;
-	Window window;
-	GC gc;
-	int isOpen;
-} linguini_X11Context;
-#endif
-
-#ifdef USE_WAYLAND
-typedef struct {
-	struct wl_compositor* compositor;
-	struct wl_shm* shm;
-
-	struct xdg_wm_base* wmBase;
-
-	struct wl_display* display;
-	struct wl_registry* registry;
-	struct wl_registry_listener registryListener;
-
-	struct wl_surface* surface;
-	struct xdg_surface* xdgSurface;
-	struct xdg_toplevel* toplevel;
-
-	unsigned char* data;
-
-	linguini_PixelArray* pixarr;
-
-	int isOpen;
-} linguini_waylandContext;
-#endif
 
 extern struct timespec lastLimitFPS;
 
@@ -129,34 +73,5 @@ void linguini_notImplemented();
 
 void linguini_useFreetype(linguini_freetypeContext* freetypeContext, const char* filepath, int charSize);
 void linguini_drawText(linguini_PixelArray* canvas, linguini_freetypeContext* freetypeContext, int x, int y, uint32_t color, const char* unformatted, ...);
-
-#ifdef USE_SDL
-void linguini_useSDL(linguini_SDLContext* sdlContext, linguini_PixelArray* canvas, const char* title);
-void linguini_closeSDL(linguini_SDLContext* sdlContext);
-
-int linguini_SDLPollEvents(SDL_Event* e);
-
-void linguini_toSDL(linguini_PixelArray* canvas, linguini_SDLContext* sdlContext);
-#endif // USE_SDL
-
-#ifdef USE_X11
-void linguini_useX11(linguini_X11Context* x11Context, linguini_PixelArray* canvas, const char* title);
-void linguini_closeX11(linguini_X11Context* x11Context);
-
-int linguini_X11NextEvent(XEvent* e, linguini_X11Context* x11Context);
-
-void linguini_toX11(linguini_PixelArray* canvas, linguini_X11Context* x11Context);
-
-#endif // USE_X11
-
-#ifdef USE_WAYLAND
-void linguini_useWayland(linguini_waylandContext* waylandContext, linguini_PixelArray* canvas, const char* title);
-void linguini_closeWayland(linguini_waylandContext* waylandContext);
-
-int linguini_waylandDisplayDispatch(linguini_waylandContext* waylandContext);
-
-static struct wl_buffer* linguini_toWayland(linguini_PixelArray* canvas, linguini_waylandContext* waylandContext);
-
-#endif // USE_WAYLAND
 
 #endif //LINGUINI_H
